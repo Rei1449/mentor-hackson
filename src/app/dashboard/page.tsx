08298bad8b3f30465e01331552ea26be8e11e941
川田明香利 <>
2024-08-18 17:59:43 +0900

    誤って投稿表示のコンポーネントを追加した部分の削除

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,9 @@
-import PostList from '@/features/post/components/post-list';
 export default async function DashBoard(){
     return(
         <>
             <div>
                 login!
             </div>
-            <PostList />
         </>
     )
 }
