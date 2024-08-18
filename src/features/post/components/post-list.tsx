c30c59e0b86ca440151c807ceb9d41472b31b50b
川田明香利 <>
2024-08-18 09:46:06 +0900

    エンドポイントの修正

--- src/features/post/components/post-list.tsx
+++ src/features/post/components/post-list.tsx
@@ -3,8 +3,8 @@ import { revalidatePath } from "next/cache";
 import { auth } from "@/libs/auth";
 
 const getPostList = async () => {
-  const res = await fetch('http://localhost:3020/api/post')
-  // const res = await fetch('http://host.docker.internal:3020/api/post'); // docker環境の場合
+  const res = await fetch('https://mentor-hackson.vercel.app/api/post')
+  // const res = await fetch('http://host.docker.internal:3020/api/post'); // ローカル環境の場合
   const json = await res.json()
   return json.posts
 }
@@ -21,8 +21,8 @@ const addPost = async (formData: FormData) => {
   const text: FormDataEntryValue | null = formData.get('text')
   if (!text) return
 
-  const res = await fetch('http://localhost:3020/api/post', {
-  // const res = await fetch('http://host.docker.internal:3020/api/post', { // docker環境の場合
+  const res = await fetch('https://mentor-hackson.vercel.app/api/post', {
+  // const res = await fetch('http://host.docker.internal:3020/api/post', { // ローカル環境の場合
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
