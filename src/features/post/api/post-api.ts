5d1ce0a198d3efd77e424524a5870dd2176c4747
川田明香利 <>
2024-08-18 10:08:20 +0900

    コンポーネントとapiの関数の分離

--- src/features/post/api/post-api.ts
+++ src/features/post/api/post-api.ts
@@ -1,5 +1,35 @@
-const getPost = async () => {
-  const res = await fetch('http://localhost:3020/api/post')
+import { revalidatePath } from "next/cache";
+import { auth } from "@/libs/auth";
+
+export const getPostList = async () => {
+  const res = await fetch('https://mentor-hackson.vercel.app/api/post')
+  // const res = await fetch('http://host.docker.internal:3020/api/post'); // ローカル環境の場合
   const json = await res.json()
   return json.posts
+}
+
+export const addPost = async (formData: FormData) => {
+  "use server"
+
+  const session = await auth();
+  const userEmail = session?.user?.email;
+  if (!userEmail) {
+    throw new Error("User not authenticated");
+  }
+  
+  const text: FormDataEntryValue | null = formData.get('text')
+  if (!text) return
+
+  const res = await fetch('https://mentor-hackson.vercel.app/api/post', {
+  // const res = await fetch('http://host.docker.internal:3020/api/post', { // ローカル環境の場合
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      body: text,
+      userEmail: userEmail
+    })
+  })
+  revalidatePath('/')
 }
--- src/features/post/components/post-list.tsx
+++ src/features/post/components/post-list.tsx
@@ -1,39 +1,5 @@
 import { Post } from '@/features/post/types/post';
-import { revalidatePath } from "next/cache";
-import { auth } from "@/libs/auth";
-
-const getPostList = async () => {
-  const res = await fetch('https://mentor-hackson.vercel.app/api/post')
-  // const res = await fetch('http://host.docker.internal:3020/api/post'); // ローカル環境の場合
-  const json = await res.json()
-  return json.posts
-}
-
-const addPost = async (formData: FormData) => {
-  "use server"
-
-  const session = await auth();
-  const userEmail = session?.user?.email;
-  if (!userEmail) {
-    throw new Error("User not authenticated");
-  }
-  
-  const text: FormDataEntryValue | null = formData.get('text')
-  if (!text) return
-
-  const res = await fetch('https://mentor-hackson.vercel.app/api/post', {
-  // const res = await fetch('http://host.docker.internal:3020/api/post', { // ローカル環境の場合
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      body: text,
-      userEmail: userEmail
-    })
-  })
-  revalidatePath('/')
-}
+import { getPostList, addPost } from '@/features/post/api/post-api';
 
 type post = {
   posts: Post[];
