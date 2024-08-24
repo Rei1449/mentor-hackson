72c51cc71e63280ccc1ad74d7438b513d3590ccb
Hiroki1211 <hiroki1211you@docomo.ne.jp>
2024-08-24 19:17:27 +0900

    forVercelError delete export

--- src/app/api/like/[id]/route.ts
+++ src/app/api/like/[id]/route.ts
@@ -6,7 +6,7 @@ import { NextResponse } from "next/server";
 const prisma = new PrismaClient();
 
 // データベースに接続する関数
-export const connect = async () => {
+const connect = async () => {
     try {
         //prismaでデータベースに接続
         prisma.$connect();
